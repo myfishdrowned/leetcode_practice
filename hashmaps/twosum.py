a0e226079b11f7d394f50003456309cb4ef00683
myfishdrowned <saif.rahman.va@gmail.com>
2025-01-26 17:21:57 -0500

    solved in python, attempting js solution

--- hashmaps/twosum.py
+++ hashmaps/twosum.py
@@ -1,8 +1,12 @@
+from collections import defaultdict
 def twosum(nums, target):
+      hashmap = defaultdict(int)
       for i, j in enumerate(nums):    
-        difference = target - i
+        difference = target - j
         if difference in hashmap:
             return [hashmap[difference], i]
-        hashmap[difference] = i
+        hashmap[j] = i
 nums = [1, 2, 4, 5]
 target = 9
+
+print(twosum(nums, target))
