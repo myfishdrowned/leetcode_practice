7cb0c8e9d19faee1d38df9db4fe7a2a0658fcdea
myfishdrowned <saif.rahman.va@gmail.com>
2025-03-08 16:04:12 -0500

    changed twosum file for practice purposes

--- hashmaps/twosum.py
+++ hashmaps/twosum.py
@@ -1,12 +1,1 @@
-from collections import defaultdict
-def twosum(nums, target):
-      hashmap = defaultdict(int)
-      for i, j in enumerate(nums):    
-        difference = target - j
-        if difference in hashmap:
-            return [hashmap[difference], i]
-        hashmap[j] = i
-nums = [1, 2, 4, 5]
-target = 6
-
-print(twosum(nums, target))
+print('hey')
